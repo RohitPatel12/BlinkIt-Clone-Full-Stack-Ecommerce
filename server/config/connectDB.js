08fa7d877f01a6ccabca8d43453a9e30c8bdb673
Rohit <itsrohitpatel45@gmail.com>
2025-09-09 03:29:35 +0530

    Deployment ready changes

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,21 +1,45 @@
+// db.js
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
 
-if(!process.env.MONGODB_URI){
-    throw new Error(
-        "Please provide MONGODB_URI in the .env file"
-    )
+dotenv.config();
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please provide MONGODB_URI in the .env file");
 }
 
-async function connectDB(){
-    try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("connect DB")
-    } catch (error) {
-        console.log("Mongodb connect error",error)
-        process.exit(1)
-    }
+// global variable to cache the connection
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export default connectDB
+async function connectDB() {
+  if (cached.conn) {
+    // ✅ Use existing connection
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    // ❌ No existing connection, create a new one
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("✅ MongoDB connected");
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
